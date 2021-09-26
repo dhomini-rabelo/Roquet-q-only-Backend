2fb2ada2a98fed1fa50fbcb2b56347cf6c9f455c
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-26 19:21:55 -0300

    Tirando copies e adptando para docker

--- project/QUESTIONS/settings.py
+++ project/QUESTIONS/settings.py
@@ -61,9 +61,13 @@ WSGI_APPLICATION = 'QUESTIONS.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
