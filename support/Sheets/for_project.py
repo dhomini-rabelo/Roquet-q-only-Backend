940ddbd515976039b797d8f8db908bdf2fe8cf4a
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-26 14:58:09 -0300

    adaptando projeto usando sheets

--- support/Sheets/for_project.py
+++ support/Sheets/for_project.py
@@ -3,16 +3,16 @@ from models.project import *
 from models.editor import Editor
 from time import sleep
 
-bp = r'C:\Users\G-fire\OneDrive\Documentos\Django\TESTE\project' # base_path
-project_name = 'TESTAGEM'
+bp = r'C:\Users\G-fire\OneDrive\Documentos\GITHUB\DJANGO\Live-Questions\project' # base_path
+project_name = 'QUESTIONS'
 project = DjangoProject(bp, project_name)
 
 
 #* APÓS CRIAÇÃO
-# delete_comments_by_folder(bp, project_name)
-# sleep(1)
-# project.adapt_urls_py()
-# sleep(1)
-# project.insert_important_comments()
-# sleep(1)
-# project.adapt_settings()
+delete_comments_by_folder(bp, project_name)
+sleep(1)
+project.adapt_urls_py()
+sleep(1)
+project.insert_important_comments()
+sleep(1)
+project.adapt_settings()
