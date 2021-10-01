9c28b16cd06184dc03ed0fe645f8b1a15b7ff6c9
Dhomini Fael <dhominisalgado@gmail.com>
2021-10-01 20:55:17 -0300

    model de pergunta e enquete

---
+++ project/asks/__init__.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class AsksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'asks'
--- project/asks/models.py
+++ project/asks/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField)
 
-# Create your models here.
+
+class Question(Model):
+    username = CharField(max_length=128)
+    text = TextField(max_length=400)
+    answered = BooleanField(default=False)
+    alteration = DateTimeField(auto_now=True)
+
+    
+class Survey(Model):
+    text = TextField(max_length=400)
+    option_1 = CharField(max_length=255, blank=True)
+    option_2 = CharField(max_length=255, blank=True)
+    option_3 = CharField(max_length=255, blank=True)
+    option_4 = CharField(max_length=255, blank=True)
+    option_5 = CharField(max_length=255, blank=True)
+    correct_option = CharField(max_length=255, blank=True)
+    finalized = BooleanField(default=False)
